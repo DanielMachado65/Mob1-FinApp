81470b512126455bcaa6e0640dbde5c9483854a2
Daniel Machado <danielmachadopintos@gmail.com>
2024-11-03 17:24:26 -0300

    feat: add new components

---
+++ app/src/main/java/com/example/mob1_finapp/RegisterOperationActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mob1_finapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterOperationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_operation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
